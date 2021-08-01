9719fbaad2509cabfe3cb5b36c9bbbc5dd9ebc7c
f-westergren <folke.westergren@gmail.com>
2021-07-31 20:52:59 -0400

    add more helpers and fix deal helper

--- helpers.js
+++ helpers.js
@@ -1,3 +1,38 @@
+const jwt = require('jsonwebtoken');
+
+const { SECRET } = require('./config');
+
+const createToken = (user) => {
+	let payload = {
+		id: user.id
+	};
+
+	return jwt.sign(payload, SECRET);
+};
+
+const partialUpdate = (table, items, key, id) => {
+	let idx = 1;
+	let columns = [];
+
+	for (let key in items) {
+		if (key.startsWith('_')) {
+			delete items[key];
+		}
+	}
+
+	for (let column in items) {
+		columns.push(`${column}=$${idx}`);
+		idx += 1;
+	}
+
+	let cols = columns.join(', ');
+	let query = `UPDATE ${table} SET ${cols} WHERE ${key}=$${idx} RETURNING *`;
+
+	let values = Object.values(items);
+	values.push(id);
+	return { query, values };
+};
+
 const shuffle = (deck) => {
 	let newDeck = JSON.parse(JSON.stringify(deck));
 	for (let i = newDeck.length - 1; i > 0; i--) {
@@ -9,18 +44,18 @@ const shuffle = (deck) => {
 	return newDeck;
 };
 
+// Deals three to each player one by one.
 const deal = (players, deck, hands) => {
-	// console.log(players, hands);
 	let cards = 0;
 	while (cards < 3) {
 		cards++;
-		for (player in players) {
-			if (!hands[player] && player !== 0) hands[player] = [];
-			if (player !== 0) {
-				hands[player].push(deck.pop());
+		for (let i = 0; i < players.length; i++) {
+			if (!hands[i] && players[i] !== 'NOPLAYER') hands[i] = [];
+			if (players[i] !== 'NOPLAYER') {
+				hands[i].push(deck.pop());
 			}
 		}
 	}
 };
 
-module.exports = { shuffle, deal };
+module.exports = { createToken, shuffle, deal, partialUpdate };
