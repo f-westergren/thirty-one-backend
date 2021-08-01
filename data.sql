39118962e73650ce07ad6018274345f328e99ee2
f-westergren <folke.westergren@gmail.com>
2021-07-31 20:53:15 -0400

    sql and package updates

--- data.sql
+++ data.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS games;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-  id integer PRIMARY KEY,
+  id text PRIMARY KEY,
   email text NOT NULL UNIQUE,
   username text NOT NULL,
   password text NOT NULL
@@ -12,17 +12,17 @@ CREATE TABLE games (
   id text PRIMARY KEY,
   deck text NOT NULL,
   pile text NOT NULL,
-  player1 integer REFERENCES users(id) ON DELETE CASCADE,
-  player2 integer REFERENCES users(id) ON DELETE CASCADE,
-  player3 integer REFERENCES users(id) ON DELETE CASCADE,
-  player4 integer REFERENCES users(id) ON DELETE CASCADE,
+  player1 text REFERENCES users(id) ON DELETE CASCADE,
+  player2 text REFERENCES users(id) ON DELETE CASCADE,
+  player3 text REFERENCES users(id) ON DELETE CASCADE,
+  player4 text REFERENCES users(id) ON DELETE CASCADE,
   hands text NOT NULL
 );
 
 INSERT INTO users 
   (id, email, username, password)
 VALUES
-  (0, 'noplayer@mail.com', 'empty', 'fogeddabutit'),
-  (1, 'aiplayer@mail.com', 'ai', 'vvvvvvsmart'),
-  (2, 'unregistered@gmail.com', 'human', 'notvvvvsmart'),
-  (3, 'folke@gmail.com', 'Folke', 'vvvvsmart')
+  ('NOPLAYER', 'noplayer@mail.com', 'empty', 'fogeddabutit'),
+  ('AI', 'aiplayer@mail.com', 'ai', 'vvvvvvsmart'),
+  ('GUEST', 'unregistered@gmail.com', 'human', 'notvvvvsmart'),
+  ('folke', 'folke@gmail.com', 'Folke', 'vvvvsmart')
